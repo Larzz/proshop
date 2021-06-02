d73f9820b2f1d9e1f3738f46b8297ba0119c6490
Larry <larry@creativouae.com>
2021-06-03 00:19:39 +0400

    convert backend syntax to es6 module from common js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -15,4 +19,7 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV;
+
+app.listen(PORT, console.log(`Server running ${NODE_ENV} on port ${PORT}`));
