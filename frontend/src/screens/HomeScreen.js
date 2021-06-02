46e7ce3352717662266944fabdf73b6265cb44e8
Larry <larry@creativouae.com>
2021-06-03 00:01:27 +0400

    remove unnessary codes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import axios from "axios";
 
